141adbbe2ee6a0f63db6aeaf24bcf477313a467b
Julio Labarca <jlabarca25@gmail.com>
2022-11-23 20:56:38 -0300

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,13 @@
+mod components;
+mod systems;
+
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    prelude::*,
-    utils::HashMap, window::PresentMode,
+    prelude::*, utils::HashMap,
 };
+use components::{State, Config, RevivedEvent, StarvedEvent, Theme, Cells, Position};
+use systems::*;
 use rand::Rng;
-
-// RESOURCES
-
-#[derive(Resource)]
-struct Config {
-    width: i32,
-    height: i32,
-    board_color: Color,
-    alive_color: Color,
-    dead_color: Color,
-}
-
-#[derive(Resource)]
-pub struct Theme {
-    pub board_color: Color,
-    pub alive_color: Color,
-    pub dead_color: Color,
-}
-
-
-#[derive(Resource)]
-struct Cells(HashMap<Coords, Entity>);
-
-// COMPONENTS
-
-#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
-struct Coords(IVec2);
-
-impl Coords {
-    fn get_neighbors(&self) -> [Coords; 8] {
-        [
-            Coords(self.0 + IVec2::new(-1, -1)),
-            Coords(self.0 + IVec2::new(0, -1)),
-            Coords(self.0 + IVec2::new(1, -1)),
-            Coords(self.0 + IVec2::new(-1, 0)),
-            Coords(self.0 + IVec2::new(1, 0)),
-            Coords(self.0 + IVec2::new(-1, 1)),
-            Coords(self.0 + IVec2::new(0, 1)),
-            Coords(self.0 + IVec2::new(1, 1)),
-        ]
-    }
-}
-
-#[derive(Component, Clone, Copy, PartialEq, Eq)]
-enum State {
-    Alive,
-    Dead,
-}
-
-struct StarvedEvent {
-    entity: Entity,
-}
-
-struct RevivedEvent {
-    entity: Entity,
-}
-
-// MAIN
 
 fn main() {
     App::new()
@@ -75,14 +21,13 @@ fn main() {
         .add_event::<RevivedEvent>()
         .add_event::<StarvedEvent>()
         .add_startup_system(setup)
-        .add_system(tick.label("Tick"))
-        .add_system(revive.after("Tick"))
-        .add_system(starve.after("Tick"))
+        .add_system(tick)
+        .add_system(revive.after(tick))
+        .add_system(starve.after(tick))
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
-                // mode: bevy::window::WindowMode::Fullscreen,
                 width: 800.0,
                 height: 800.0,
                 ..default()
@@ -92,12 +37,7 @@ fn main() {
         .run();
 }
 
-// SYSTEMS
-fn setup(
-    mut commands: Commands,
-    windows: Res<Windows>,
-    config: Res<Config>,
-) {
+fn setup(mut commands: Commands, windows: Res<Windows>, config: Res<Config>) {
     let theme = Theme {
         board_color: config.board_color,
         alive_color: config.alive_color,
@@ -112,13 +52,14 @@ fn setup(
 
     let center_cell = Vec2::new(config.width as f32, config.height as f32) / 2.0 * cell_size;
 
-    commands.spawn(Camera2dBundle::default())
-            .insert(Transform::from_translation(center_cell.extend(999.9)));
+    commands
+        .spawn(Camera2dBundle::default())
+        .insert(Transform::from_translation(center_cell.extend(999.9)));
 
     commands.spawn(SpriteBundle {
         transform: Transform::from_translation(center_cell.extend(0.0)),
         sprite: Sprite {
-            custom_size: Some(Vec2::splat(screen_height as f32) + Vec2::splat(2.0)),
+            custom_size: Some(Vec2::splat(screen_height) + Vec2::splat(2.0)),
             color: theme.board_color,
             ..Default::default()
         },
@@ -154,73 +95,13 @@ fn setup(
                     ..Default::default()
                 })
                 .insert(state)
-                .insert(Coords(cell))
+                .insert(Position(cell))
                 .id();
 
-            cells.0.insert(Coords(cell), entity);
+            cells.0.insert(Position(cell), entity);
         }
     }
 
     commands.insert_resource(theme);
     commands.insert_resource(cells);
-}
-
-fn tick(
-    mut starved_writer: EventWriter<StarvedEvent>,
-    mut revived_writer: EventWriter<RevivedEvent>,
-    cells: Res<Cells>,
-    query: Query<&State>,
-) {
-    for (coords, entity) in cells.0.iter() {
-        let mut alive_count = 0;
-
-        for n_coords in coords.get_neighbors().iter() {
-            if let Some(n_entity) = cells.0.get(n_coords) {
-                if let Ok(state) = query.get(*n_entity) {
-                    if *state == State::Alive {
-                        alive_count += 1;
-                    }
-                }
-            }
-        }
-
-        match query.get(*entity).unwrap() {
-            State::Alive => {
-                if alive_count > 3 || alive_count < 2 {
-                    starved_writer.send(StarvedEvent { entity: *entity });
-                }
-            }
-            State::Dead => {
-                if alive_count == 3 {
-                    revived_writer.send(RevivedEvent { entity: *entity });
-                }
-            }
-        }
-    }
-}
-
-fn revive(
-    theme: Res<Theme>,
-    mut reader: EventReader<RevivedEvent>,
-    mut query: Query<(&mut State, &mut Sprite)>,
-) {
-    for event in reader.iter() {
-        if let Ok((mut state, mut sprite)) = query.get_mut(event.entity) {
-            *state = State::Alive;
-            sprite.color = theme.alive_color;
-        }
-    }
-}
-
-fn starve(
-    theme: Res<Theme>,
-    mut reader: EventReader<StarvedEvent>,
-    mut query: Query<(&mut State, &mut Sprite)>,
-) {
-    for event in reader.iter() {
-        if let Ok((mut state, mut sprite)) = query.get_mut(event.entity) {
-            *state = State::Dead;
-            sprite.color = theme.dead_color;
-        }
-    }
 }
